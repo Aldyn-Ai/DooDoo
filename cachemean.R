4d92d0e74be9b3afd17044e87042c61a2b90af63
Aldyn-Ai <44164758+Aldyn-Ai@users.noreply.github.com>
2018-10-30 11:29:03 +0300

    Update cachemean.R

--- cachemean.R
+++ cachemean.R
@@ -1,26 +1,47 @@
-makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL
-        set <- function(y) {
-                x <<- y
-                inv <<- NULL
-        }
-        get <- function() x
-        setInverse <- function(inverse) inv <<- inverse
-        getInverse <- function() inv
-        list(set = set,
-             get = get,
-             setInverse = setInverse,
-             getInverse = getInverse)
+## The functions reperesented below do the caching and
+## inverse of a generic matrix.
+
+## Function is responsible for object creation that is capable
+## of caching matrix object and its inverse.
+
+makeCacheMatrix <- function(mx = matrix()) {
+	imx <- NULL
+	
+	get <- function() return(mx)
+	
+	getInverse <- function() return(imx)
+	
+	set <- function(val) {
+		mx <<- val
+		inverse <<- NULL
+	}
+	
+	setInverse <- function(val) imx <<- val
+	
+	return(
+		list(
+			set = set,
+			get = get,
+			setInverse = setInverse,
+			getInverse = getInverse
+		)	
+	)
 }
 
-cacheSolve <- function(x, ...) {
-        inv <- x$getInverse()
-        if (!is.null(inv)) {
-                message("getting cached data")
-                return(inv)
-        }
-        mat <- x$get()
-        inv <- solve(mat, ...)
-        x$setInverse(inv)
-        inv
+
+## Function gets inverse from cached matrix.
+## In case cached matrix is null null is returned.
+## If inverted matrix is not present in cache, its
+## value is calculated and cached.
+
+cacheSolve <- function(cmx, ...) {
+	imx <- cmx$getInverse()
+	
+	if (!is.null(imx)) return(imx)
+	
+	mx <- cmx$get()
+	imx <- solve(mx, ...)
+	cmx$setInverse(imx)
+	
+	return(imx)
 }
